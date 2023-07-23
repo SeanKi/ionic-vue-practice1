ac7cacb29d728fd15e90f25a151d0475f3eeb29f
SeanKi <ykish72@gmail.com>
2023-07-23 20:42:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-vue-practice1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/c3pvy4)
---
+++ src/components/Example.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>Header Toolbar</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content color="light">
+    <ion-list :inset="true">
+      <ion-item>
+        <ion-label>Pokémon Yellow</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Mega Man X</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>The Legend of Zelda</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Pac-Man</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Super Mario World</ion-label>
+      </ion-item>
+    </ion-list>
+  </ion-content>
+  <ion-footer>
+  <ion-toolbar>
+    <ion-title>Footer</ion-title>
+  </ion-toolbar>
+</ion-footer>
+</template>
+
+<script lang="ts">
+import { IonContent, IonItem, IonLabel, IonList } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { IonContent, IonItem, IonLabel, IonList },
+});
+</script>
